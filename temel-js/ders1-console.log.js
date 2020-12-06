39f372f41a0c9870ad52af0cd4d86cb470f9d704
kemalist <guray.turan@outlook.com>
2020-12-06 22:55:07 +0300

    test

---
+++ temel-js/ders1-console.log.js
@@ -0,0 +1,2 @@
+// terminalden çalıştırma  --> node ders1-console.log.js
+console.log("Selam dostum nasılssın?")
---
+++ temel-js/ders2-variables.js
@@ -0,0 +1,16 @@
+var dataCount =8,
+    db="mongodb"
+;
+console.log(dataCount);
+
+/*
+*
+* Açıklama yazmak için kullanılır
+*
+ */
+var sayı =2;
+console.log(sayı +"3");
+
+console.log("değişkenin sonuna ++ ekleme: "+sayı++);
+sayı =2;
+console.log("değişkenin başına ++ ekleme: "+ ++sayı)
